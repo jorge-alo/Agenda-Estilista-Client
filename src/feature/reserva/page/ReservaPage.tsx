f5546b3e0fdd707ca65ac84d3e0648cf52f881fe
Jorge Altamirano <alojorge13@gmail.com>
2026-08-18 11:51:43 -0300

    corrigiendo s por c

--- src/feature/reserva/page/ReservaPage.tsx
+++ src/feature/reserva/page/ReservaPage.tsx
@@ -165,7 +165,7 @@ export const ReservaPage = () => {
       <ErrorBoundary>
         <div className="rp-body">
           <div className="rp-step" key="estilista-step">
-            <p className="rp-step-label">Profecionales</p>
+            <p className="rp-step-label">Profesionales</p>
             <Estilistas slug={slug} estilistaId={estilistaId} setEstilistaId={setEstilistaId} />
           </div>
 
